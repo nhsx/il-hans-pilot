6b2bad9934c4680a54753191a1a6040cf9d56a4e
Henry Wright <henry.wright@thepsc.co.uk>
2023-04-13 17:29:48 +0100

    HANS-303: Removed env configuration for logfile and errorlog

--- management_interface/start.sh
+++ management_interface/start.sh
@@ -9,4 +9,4 @@ echo "Starting Nginx"
 service nginx start
 
 echo "Starting Gunicorn"
-gunicorn management_interface.wsgi:application --bind 0.0.0.0:8000 --timeout 120 --workers $GUNICORN_WORKERS --log-level $GUNICORN_LOGLEVEL --access-logfile $GUNICORN_ACCESSLOG --error-logfile $GUNICORN_ERRORLOG
+gunicorn management_interface.wsgi:application --bind 0.0.0.0:8000 --timeout 120 --workers $GUNICORN_WORKERS --log-level $GUNICORN_LOGLEVEL --access-logfile - --error-logfile -
